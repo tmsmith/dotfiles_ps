73ebf43c0423d8dc191a6adb18a843c64a0964cf
tmsmith <thad@acst.com>
2016-12-07 09:36:46 -0500

    switched form Jump-Location to posz

--- home/profile.ps1
+++ home/profile.ps1
@@ -6,7 +6,5 @@ ls "$scriptPath\*.ps1" -exclude profile.*,path.* | %{
   . (Resolve-Path $_)
 }
 
-Import-Module "$scriptPath\jump-location\Jump.Location.psd1"
-
 Set-PSReadlineOption -TokenKind Command -ForegroundColor Gray
 Set-PSReadlineOption -TokenKind Parameter -ForegroundColor Yellow
